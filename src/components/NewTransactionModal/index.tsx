1ff5d667b57ba866943accd800b86daddb99cee9
Thiago Nunes <msndothiago@live.com>
2022-05-12 19:20:17 -0300

    second commit

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,10 @@
 import Modal from 'react-modal'
-import { Container } from './styles';
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
 import closeImg from '../../assets/Close.svg';
+import incomeImg from '../../assets/Entradas.svg';
+import outcomeImg from '../../assets/Saidas.svg';
+import { useState } from 'react';
+
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -8,6 +12,9 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const [type, setType] = useState('deposit');
+
+       
     return(
         <Modal 
             isOpen={isOpen} 
@@ -34,6 +41,30 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                     type="number"
                     placeholder="Valor"
                 />
+
+                <TransactionTypeContainer> 
+                <RadioBox
+                    type="button"
+                    onClick={() => { setType('deposit')}}
+                    isActive={type === 'deposit'}
+                    activeColor="green"                    
+                >
+                    <img src={incomeImg} alt="Entrada" />
+                    <span>Entrada</span>
+
+                </RadioBox>
+                <RadioBox
+                    type="button"
+                    onClick={() => { setType('withdraw')}}                   
+                    isActive={type === 'withdraw'}  
+                    activeColor="red"                  
+                >
+                    <img src={outcomeImg} alt="Saída" />
+                    <span>Entrada</span>
+
+                </RadioBox>
+
+                </TransactionTypeContainer>
 
                 <input 
                     placeholder="Categoria"
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { darken, transparentize } from 'polished'; //com o polished é possivel utilizar funcoes dentro do TS css. ex. o Darken que vai escurecer o botao.
 
 export const Container = styled.div`
     h2 {
@@ -51,5 +52,58 @@ export const Container = styled.div`
         }
         }
 
+
+
+`;
+
+export const TransactionTypeContainer = styled.div`
+    margin: 1rem 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 0.5rem;
+
+`;
+
+interface RadioBoxProps {
+    isActive: boolean;
+    activeColor: 'green' | 'red';
+}
+
+const colors = {
+    green: '#33CC95',
+    red: '#E52E40'
+};
+
+export const RadioBox = styled.button<RadioBoxProps>`
+        
+        height: 4rem;
+        border: 1px solid #d7d7d7;
+        border-radius: 0.25rem;
+
+        background: ${(props) => props.isActive 
+        ? transparentize(0.9, colors[props.activeColor]) 
+        : 'transparent'};
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        transition: border-color 0.2s;
+
+        &:hover {
+            border-color: ${darken(0.1, '#d7d7d7')};
+        }
+
+        img {
+            width: 20px;
+            height: 20px;
+        }
+
+        span {
+            display: inline-block;
+            margin-left: 1rem;
+            font-size: 1rem;
+            color: var(--text-title);
+        }
 
 `;
